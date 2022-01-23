71bf65547bb68d51792e28aaaaa48341600ce62e
DESKTOP-C8SL4R7\kosmo <tora-ng>
2022-01-23 15:35:34 +0900

    member/member로 로그인 가능하게, loginForm2으로 로그인 페이지 커스터마이징 한 것까지 커밋 ^^

--- src/main/java/edu/kosmo/ysy/HomeController.java
+++ src/main/java/edu/kosmo/ysy/HomeController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,4 +37,11 @@ public class HomeController {
 		return "home";
 	}
 	
+	@GetMapping("/login/loginForm")
+	public String loginForm() {
+		
+		System.out.println("Welcome Login Form");
+		
+		return "login/loginForm2";
+	}
 }
