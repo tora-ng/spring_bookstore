d2f79c8d631d5acc3e00779fc71d5a06defa069a
DESKTOP-C8SL4R7\kosmo <tora-ng>
2022-01-27 10:26:34 +0900

    commit 암호화 후 로그인 되는 부분까지

--- src/main/java/edu/kosmo/ysy/mapper/MemberMapper.java
+++ src/main/java/edu/kosmo/ysy/mapper/MemberMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+
 import edu.kosmo.ysy.vo.AuthVO;
 import edu.kosmo.ysy.vo.MemberVO;
 
@@ -13,6 +14,9 @@ public interface MemberMapper {
 	
 	@Select("SELECT authority, userid FROM member, authority where member.userid = authority.userid and member.userid = #{USERID}")	
 	public List<AuthVO> readAuthority(String userid);
+	
+	@Insert("insert into member(userid,userpassword) values(#{userid},#{userpassword})")
+	public int insertMember(MemberVO memberVO);
 	
 	
 }
